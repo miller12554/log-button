21b89c376d796b7e7e9e1a763f5c15b09eb4be02
miller12554 <53585141+miller12554@users.noreply.github.com>
2023-06-12 19:26:15 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./styles.scss";
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { isLoggedIn: true };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((prevState) => {
+      return { isLoggedIn: !prevState.isLoggedIn };
+      // negate whatever isLoggedIn returns
+    });
+  }
+
+  render() {
+    let currentStatus = this.state.isLoggedIn ? "logged in" : "logged out";
+    let buttonStatus = this.state.isLoggedIn ? "Log Out" : "Log In";
+
+    return (
+      <div>
+        <h1> You're currently {currentStatus} </h1>
+        <button className="log-in" onClick={this.handleClick}>
+          {buttonStatus}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
